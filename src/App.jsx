ca6e7373fc2f927cca771c7d411a89ee2c168145
JihyeKim <jihyekim830@gmail.com>
2025-10-17 15:48:41 +0900

    feat: 17일차 과제

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React from "react";
-import "./App.css";
-import TestComponent from "./TestComponent";
+import { lazy, Suspense } from 'react';
+import Loading from '@components/Loading';
+const TestComponent = lazy(() => import('@components/TestComponent'));
+import '@styles/App.css';
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
         lazy와 Suspense
         <br /> 사용해보기
       </h1>
-      <TestComponent />
+      <Suspense fallback={<Loading />}>
+        <TestComponent />
+      </Suspense>
     </div>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': resolve(__dirname, './src/components'),
+      '@styles': resolve(__dirname, './src/styles'),
+    },
+  },
 });
